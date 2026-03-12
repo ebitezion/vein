554f895235690858a5f52281bd0b618d2ee85d80
Ebite Zion <ebitezion@gmail.com>
2026-03-12 18:03:03 +0100

    Added the validator package, DB, and golang-migrate

---
+++ .history/cmd/api/helpers_20260311201459.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+// envelope JSON responses behind a wrapper
+type envelope map[string]interface{}
+
+// Read ID param
+func readIDParam(r *http.Request) string {
+	//get the request context
+	params := httprouter.ParamsFromContext(r.Context())
+	return params.ByName("ID")
+}
+
+// writeJSON writes a formats our response to a json
+func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
+
+	//Receive input and conv to map format
+	resp, err := json.Marshal(data)
+
+	//set header to accept application/json
+	for k, v := range headers {
+		w.Header()[k] = v
+	}
+
+	resp = append(resp, '\n')
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(resp)
+
+	return err
+
+}
+
+
+func (app *application)readJSON(w http.ResponseWriter, r *http.Request, data interface{})error  {
+	json.NewDecoder(r.)	
+}
--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.24.2
 require (
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/julienschmidt/httprouter v1.3.0 // indirect
+	github.com/lib/pq v1.11.2
 )
